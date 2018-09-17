dfd60e31f508f91723f207929909a2856902016e
kazumasa.fujita <kazumasa.fuji@gmail.com>
2018-09-18 08:30:56 +0900

    modify error

--- get_company.py
+++ get_company.py
@@ -11,7 +11,6 @@ def is_pagenation(page):
     soup = BeautifulSoup(r.text, 'lxml')
     next_page = soup.find('li', class_='next')
     link = next_page.find('a')
-    print(link.get('href'))
     if link:
         return True
     else:
@@ -20,19 +19,26 @@ def is_pagenation(page):
 
 get_url = []
 # URLを取得する
-for i in range(1,):
+for i in range(1,300):
 
+    # 時間を追加
+    sleep(1)
     if is_pagenation(i):
-        start_url = 'https://creww.me/ja/search/startup?page=' + str(i)
-        r = requests.get(start_url)
-        soup = BeautifulSoup(r.text, 'lxml')
-        startup_block = soup.find_all('div', class_='startup-block')
-        
-        # 企業のリンクを取得して、配列に格納する
-        # blockはだいたい18個
-        for x in range(len(startup_block)):
-            startup_link = startup_block[x].find('div', class_='startup-name')
-            get_url.append(startup_link.find('a').get('href'))
+        try:
+            start_url = 'https://creww.me/ja/search/startup?page=' + str(i)
+            print(start_url)
+            r = requests.get(start_url)
+            soup = BeautifulSoup(r.text, 'lxml')
+            startup_block = soup.find_all('div', class_='startup-block')
+            
+            # 企業のリンクを取得して、配列に格納する
+            # blockはだいたい18個
+            for x in range(len(startup_block)):
+                startup_link = startup_block[x].find('div', class_='startup-name')
+                get_url.append(startup_link.find('a').get('href'))
+        except Exception:
+            print('try except error')
+            break
     else:
         print('pagenatino end --------------------------')
         break
